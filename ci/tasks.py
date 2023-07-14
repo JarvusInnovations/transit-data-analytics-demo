f980c52cb6b1374f2a138f98f78610fafb93f6e6
Andrew Vaccaro <atvaccaro@gmail.com>
2023-07-14 17:35:01 -0400

    add other infra, add diff/upgrade/install

--- ci/tasks.py
+++ ci/tasks.py
@@ -2,8 +2,11 @@ from enum import StrEnum
 from pathlib import Path
 from typing import List, Optional
 
-from invoke import task, Context, UnexpectedExit
+from invoke import task, Context, Result
 from pydantic import BaseModel, parse_obj_as
+
+
+# TODO: use GitPython to form absolute paths
 
 
 class Driver(StrEnum):
@@ -13,7 +16,7 @@ class Driver(StrEnum):
 
 class Deployment(BaseModel):
     name: str
-    namespace: Optional[str] = None
+    namespace: str
     driver: Driver
 
     # helm specific
@@ -29,7 +32,7 @@ class Deployment(BaseModel):
 
     @property
     def values_cli(self) -> str:
-        return " ".join(f"-f {values}" for values in self.values)
+        return " ".join(f"-f ../{values}" for values in self.values)
 
 
 class JarvusConfig(BaseModel):
@@ -37,25 +40,36 @@ class JarvusConfig(BaseModel):
 
 
 @task
+def helm_plugins(c):
+    c.run("helm plugin install https://github.com/databus23/helm-diff", warn=True)
+
+
+@task
 def parse_jarvus_config(c: Context):
     c.update({"jarvus_config": parse_obj_as(JarvusConfig, c.config["jarvus"]._config)})
 
 
-@task(parse_jarvus_config)
-def hdiff(c):
-    # c.run(f"helm diff upgrade {}")
-    pass
-
-
-@task(parse_jarvus_config)
-def happly(c):
+@task(helm_plugins, parse_jarvus_config)
+def hdiff(c, name=None):
     for deployment in c.config.jarvus_config.deployments:
-        c.run(f"helm dependency build ../{deployment.chart}")
+        if not name or name == deployment.name:
+            c.run(
+                f"helm diff upgrade {deployment.name} ../{deployment.chart} {deployment.namespace_cli} {deployment.values_cli}"
+            )
 
-        try:
-            c.run(f"kubectl get ns {deployment.namespace}")
-            # c.run(f"helm upgrade {deployment.name} ../{deployment.chart} {deployment.namespace_cli} {deployment.values_cli}")
-        except UnexpectedExit as e:
-            assert f'namespaces "{deployment.namespace}" not found' in e
-            c.run(f"kubectl create ns {deployment.namespace}")
-            # c.run(f"helm install {deployment.name} ../{deployment.chart} {deployment.namespace_cli} {deployment.values_cli}")
+
+@task(helm_plugins, parse_jarvus_config)
+def happly(c, name=None):
+    for deployment in c.config.jarvus_config.deployments:
+        if not name or name == deployment.name:
+            c.run(f"helm dependency build ../{deployment.chart}")
+
+            res: Result = c.run(f"kubectl get ns {deployment.namespace}", warn=True)
+
+            if res.exited:
+                assert f'namespaces "{deployment.namespace}" not found' in res.stderr
+                c.run(f"kubectl create ns {deployment.namespace}")
+
+            c.run(
+                f"helm upgrade --install {deployment.name} ../{deployment.chart} {deployment.namespace_cli} {deployment.values_cli}"
+            )
