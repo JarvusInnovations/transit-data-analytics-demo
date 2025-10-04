bd301cc36213ee95876afa7c938bb0040ab6ab76
Laurie Merrell <laurie.m@jarv.us>
2025-10-03 20:05:23 -0500

    feat: add vehicle position fields to staging model

--- warehouse/models/staging/stg_gtfs_rt__vehicle_positions.sql
+++ warehouse/models/staging/stg_gtfs_rt__vehicle_positions.sql
@@ -5,7 +5,16 @@ WITH src AS (
 unpack_json AS (
     SELECT
         {{ read_file_config_and_partitions() }},
-        {{ read_gtfs_rt_trip_descriptor('entity.vehicle') }}
+        {{ read_gtfs_rt_trip_descriptor('entity.vehicle') }},
+        JSON_VALUE(record, '$.entity.vehicle.position.latitude') AS latitude,
+        JSON_VALUE(record, '$.entity.vehicle.position.longitude') AS longitude,
+        JSON_VALUE(record, '$.entity.vehicle.position.bearing') AS bearing,
+        JSON_VALUE(record, '$.entity.vehicle.position.odometer') AS odometer,
+        JSON_VALUE(record, '$.entity.vehicle.position.speed') AS speed,
+        JSON_VALUE(record, '$.entity.stopId') AS stop_id,
+        JSON_VALUE(record, '$.entity.timestamp') AS vehicle_timestamp,
+        JSON_VALUE(record, '$.entity.current_stop_sequence') AS current_stop_sequence,
+        JSON_VALUE(record, '$.entity.current_status') AS current_status
     FROM src
 
 ),
@@ -13,7 +22,16 @@ unpack_json AS (
 stg_gtfs_rt__vehicle_positions AS (
     SELECT
         {{ metadata_columns() }},
-        {{ trip_descriptor_columns() }}
+        {{ trip_descriptor_columns() }},
+        latitude,
+        longitude,
+        bearing,
+        odometer,
+        speed,
+        stop_id,
+        vehicle_timestamp,
+        current_stop_sequence,
+        current_status
     FROM unpack_json
 )
 
