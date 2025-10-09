1feb0fca9d6afa8c4db79ce8f0171c9b9e7aba65
Laurie Merrell <laurie.m@jarv.us>
2025-10-08 21:17:22 -0500

    add shape percentage to fct scheduled stops

--- warehouse/models/mart/fct_scheduled_stops.sql
+++ warehouse/models/mart/fct_scheduled_stops.sql
@@ -9,13 +9,20 @@ with stop_times as (
 ),
 
 stops as (
-    select * from {{ ref('stg_stops') }}
+    select
+        *,
+        st_geogpoint(stop_lon, stop_lat) as stop_pt
+    from {{ ref('stg_stops') }}
 ),
 
 trips as (
     select * from {{ ref('fct_scheduled_trips') }}
     -- todo: remove this filter
     where dt = '2024-02-08'
+),
+
+shapes as (
+    select * from {{ ref('dim_shapes') }}
 ),
 
 fct_scheduled_stops as (
@@ -29,10 +36,13 @@ fct_scheduled_stops as (
         trips.service_id,
         trips.route_id,
         trips.route_type,
-        stops.stop_lat,
-        stops.stop_lon,
-        MAKE_INTERVAL(second => stop_times.arrival_secs) + trips.service_date as arrival_time,
-        MAKE_INTERVAL(second => stop_times.departure_secs) + trips.service_date as departure_time
+        stops.stop_pt,
+        st_closestpoint(shapes.shape_linestring, stops.stop_pt) as shape_closest_point_to_stop,
+        case
+            when not st_iscollection(shapes.shape_linestring) then st_linelocatepoint(shapes.shape_linestring, st_closestpoint(shapes.shape_linestring, stops.stop_pt))
+        end as shape_closest_point_to_stop_as_pct,
+        make_interval(second => stop_times.arrival_secs) + trips.service_date as arrival_time,
+        make_interval(second => stop_times.departure_secs) + trips.service_date as departure_time
     from trips
     left join stop_times
         on
@@ -44,6 +54,11 @@ fct_scheduled_stops as (
             trips._b64_url = stops._b64_url
             and trips.dt = stops.dt
             and stop_times.stop_id = stops.stop_id
+    left join shapes
+        on
+            trips._b64_url = shapes._b64_url
+            and trips.dt = shapes._valid_from
+            and trips.shape_id = shapes.shape_id
 )
 
 select * from fct_scheduled_stops
