0d5160ad9840178dfd4d61f7187c895b3131614b
Laurie Merrell <laurie.m@jarv.us>
2025-10-23 17:24:54 -0500

    refactor: split list of segments from their geographies

--- warehouse/models/mart/fct_observed_segments.sql
+++ warehouse/models/mart/fct_observed_segments.sql
@@ -4,40 +4,38 @@ with stop_times as (
     select * from {{ ref('fct_observed_stop_times') }}
 ),
 
-shapes as (
-    select * from {{ ref('dim_shapes') }}
-),
-
 make_segments as (
     select distinct
+        feed_key,
+        feed_name,
         schedule_b64_url,
         schedule_dt,
         stop_id,
+        stop_name,
         shape_id,
         shape_closest_point_to_stop_as_pct,
-        lead(shape_closest_point_to_stop_as_pct) over (partition by schedule_b64_url, trip_id, service_date order by scheduled_stop_sequence) as next_stop_pct,
-        lead(shape_id) over (partition by schedule_b64_url, trip_id, service_date order by scheduled_stop_sequence) as next_stop_id
+        lead(shape_closest_point_to_stop_as_pct) over (partition by schedule_b64_url, schedule_dt, trip_id, service_date order by scheduled_stop_sequence) as next_stop_pct,
+        lead(shape_id) over (partition by schedule_b64_url, schedule_dt, trip_id, service_date order by scheduled_stop_sequence) as next_stop_id,
+        lead(stop_name) over (partition by schedule_b64_url, schedule_dt, trip_id, service_date order by scheduled_stop_sequence) as next_stop_name
     from stop_times
 ),
 
 fct_observed_segments as (
     select --noqa: ST06
-        make_segments.shape_id || "-" || make_segments.next_stop_id as segment_id,
-        make_segments.schedule_b64_url,
-        make_segments.schedule_dt,
-        make_segments.stop_id,
-        make_segments.shape_id,
-        make_segments.shape_closest_point_to_stop_as_pct,
-        make_segments.next_stop_pct,
-        make_segments.next_stop_id,
-        st_linesubstring(shapes.shape_linestring, make_segments.shape_closest_point_to_stop_as_pct, make_segments.next_stop_pct) as segment_linestring
+        feed_key,
+        feed_name,
+        stop_id || "-" || next_stop_id as segment_id,
+        stop_name || "-" || next_stop_name as segment_name,
+        schedule_b64_url,
+        schedule_dt,
+        stop_id,
+        shape_id,
+        stop_name,
+        shape_closest_point_to_stop_as_pct,
+        next_stop_pct,
+        next_stop_id,
+        next_stop_name
     from make_segments
-    left join shapes
-        on
-            make_segments.schedule_b64_url = shapes._b64_url
-            and make_segments.schedule_dt = shapes._valid_from
-            and make_segments.shape_id = shapes.shape_id
-    where make_segments.shape_closest_point_to_stop_as_pct < make_segments.next_stop_pct
 )
 
 select * from fct_observed_segments
